3ef2ae2da2773e7b4ece5838391fcc485280784d
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2019-11-20 22:11:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -164,6 +164,20 @@ async def on_message(message):
         await message.author.send(" Official Server https://discord.gg/SzffkfK ")
         msg = ' ' + author + '. Netflix Accounts : '
         await message.author.send(msg + (random.choice(randomlist)))
+        
+if message.content.startswith('+nordvpn'):
+        randomlist = ['   ']
+        await message.author.send("Note: We have added Little Advertisement.")
+        
+        await message.author.send("You Can Get more than 50 Accounts by seeing advertisement 1 time.")
+        await message.author.send(" Watch Advertisement and Recieve Alot Accounts Thank You.")
+        await message.author.send(":one: ( https://link-to.net/41622/nord ) 150x NordVPN Accounts list 1.")
+        await message.author.send(":two: ( https://link-to.net/41622/norde2 ) 150x NordVPN Accounts list 2.")
+        await message.author.send(":three: ( https://link-to.net/41622/norde3 ) 200x nordVPN Accounts list 3.")
+        await message.author.send(" Official Server https://discord.gg/SzffkfK ")
+        msg = ' ' + author + '. NordVPN Accounts : '
+        await message.author.send(msg + (random.choice(randomlist)))
+        
 
 
 
