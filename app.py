72e0a8a3ff25009fdcf75ffc9a532ee295312150
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2021-05-13 20:15:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ Clientdiscord = discord.Client()
 
 
 #create an arraylist containing phrases you want your bot to switch through.
-status = cycle(['+help', '+stock', '+spotify', '+origin', '+netflix', '+hulu', '+minecraft', '+nitro', '+mailaccess'])
+status = cycle(['+help'])
 
 client.remove_command('help')
 
@@ -61,83 +61,6 @@ async def on_message(message):
         await message.author.send("```Available Stocks 17 November```")
         await message.author.send(" Official Server https://discord.gg/SzffkfK ")
 
-    if message.content.startswith('+mailaccess'):
-        randomlist = ['   ']
-        await message.author.send("Note: We have added Little Advertisement.")
-        await message.author.send(" You Can Get more than 50 Accounts by seeing advertisement 1 time. ")
-        await message.author.send("Watch Advertisement and Recieve Alot Accounts Thank You.")
-        await message.author.send(" :one: ( https://link-to.net/41622/mailacc1 ) 100x Mail Access Accounts list 1. ")
-        await message.author.send(" :two: ( https://link-to.net/41622/mail2 ) 100x Mail Access Accounts list 2.")
-        await message.author.send("  :three: ( https://link-to.net/41622/mailaccu3 ) 150x Mail Access Accounts list 3. ")
-        await message.author.send("  :four: ( https://link-to.net/41622/mailaccc4 ) 150x Mail Access Accounts list 4 ")
-        await message.author.send(" Join Tabish Gen Official CM** https://discord.gg/SzffkfK ")  
-        msg = ' ' + author + '. Mail Access Accounts : '
-        await message.author.send(msg + (random.choice(randomlist)))
-
-
-    if message.content.startswith('+minecraft'):
-        randomlist = ['']
-        await message.author.send("Note: We have added Little Advertisement.")
-        await message.author.send(" You Can Get more than 50 Accounts by seeing advertisement 1 time. ")
-        await message.author.send("Watch Advertisement and Recieve Alot Accounts Thank You.")
-        await message.author.send(" :one: ( https://up-to-down.net/41622/mineas1 ) 120x Minecraft Accounts list 1. ")
-        await message.author.send(":two: ( https://up-to-down.net/41622/minesd2 ) 120x Minecraft Accounts list 2.")
-        await message.author.send(" :three: ( https://up-to-down.net/41622/minets3 ) 149x Minecraft Accounts list 3 ")
-        await message.author.send(" Join Tabish Gen Official CM** https://discord.gg/SzffkfK ")  
-        msg = ' ' + author + '. Minecraft Accounts : '
-        await message.author.send(msg + (random.choice(randomlist)))
-
-
-    if message.content.startswith('+hulu'):
-        randomlist = ['']
-
-        await message.author.send("Note: We have added Little Advertisement.")
-        await message.author.send(" You Can Get more than 50 Accounts by seeing advertisement 1 time. ")
-        await message.author.send("Watch Advertisement and Recieve Alot Accounts Thank You.")
-        await message.author.send(" :one: ( https://link-to.net/41622/hulu11 ) 1000x Hulu Accounts list 1. ")
-        await message.author.send(":two: ( https://link-to.net/41622/hul2 ) 1000x Hulu Accounts list 2.")
-        await message.author.send(" :three: ( https://link-to.net/41622/hulu33 ) 1111x Hulu Accounts list 3 ")        
-        await message.author.send(" Official Server https://discord.gg/SzffkfK ")
-        msg = ' ' + author + '. Hulu Account : '
-        await message.author.send(msg + (random.choice(randomlist)))
-
-    if message.content.startswith('+nitro'):
-        randomlist = [' ']
-        await message.author.send("Note: We have added Little Advertisement.")
-        await message.author.send("You Can Get more than 50 Accounts by seeing advertisement 1 time.")
-        await message.author.send("Watch Advertisement and Recieve Alot Accounts Thank You.")
-        await message.author.send(":one: ( https://link-to.net/41622/code1 ) 200x Nitro Codes list 1.")
-        await message.author.send(":two: ( https://link-to.net/41622/codes2 ) 200x Nitro Codes list 2.")
-        await message.author.send(":three: ( https://link-to.net/41622/coded3 ) 200x Nitro Codes list 3.")
-        await message.author.send(":four: ( https://link-to.net/41622/coddes4 ) 240x Nitro Codes list 4")
-        await message.author.send(" Join Tabish Gen Official CM** https://discord.gg/SzffkfK ")  
-        msg = ' ' + author + '. Nitro Codes : '
-        await message.author.send(msg + (random.choice(randomlist)))
-
-    if message.content.startswith('+crunchyroll'):
-        randomlist = [' ']
-        await message.author.send("Note: We have added Little Advertisement.")
-        await message.author.send("You Can Get more than 50 Accounts by seeing advertisement 1 time .")
-        await message.author.send("Watch Advertisement and Recieve Alot Accounts Thank You.")
-        await message.author.send(":one: ( https://link-to.net/41622/crunchy1 ). 60x Crunchyroll Accounts list 1")
-        await message.author.send(":two: ( https://link-to.net/41622/crunch2 ). 67x Crunchyroll Accounts list 2")
-        await message.author.send(" Join Tabish Gen Official CM** https://discord.gg/SzffkfK ") 
-        msg = ' ' + author + '. Crunchyroll Accounts : '
-        await message.author.send(msg + (random.choice(randomlist)))
-
-    if message.content.startswith('+netflix'):
-        randomlist = ['      ']
-        await message.author.send("Note: We have added Little Advertisement."
-        await message.author.send("You Can Get more than 50 Accounts by seeing advertisement 1 time.")
-        await message.author.send(" Watch Advertisement and Recieve Alot Accounts Thank You.")
-        await message.author.send(":one: ( https://link-to.net/41622/netflixe ) 200x Netflix Accounts list 1.")
-        await message.author.send(":two: ( https://link-to.net/41622/netfl2 ) 200x Netflix Accounts list 2.")
-        await message.author.send(":three: ( https://link-to.net/41622/netfli3 ) 200x Netflix Accounts list 3.")
-        await message.author.send(":four: ( https://link-to.net/41622/netfliee4 ) 260x Netflix Accounts list 4.")
-        await message.author.send(":five: ( https://link-to.net/41622/nefliexx5 ) 303x Netflix Accounts list 5")
-        await message.author.send(" Official Server https://discord.gg/SzffkfK ")
-        msg = ' ' + author + '. Netflix Accounts : '
-        await message.author.send(msg + (random.choice(randomlist)))
                                   
 
                                   
